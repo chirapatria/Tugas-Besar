f543a4ef81bd8f05ce37d011f78da45e67a073a6
chirapatria <screamathica.band@gmail.com>
2015-12-21 04:17:11 +0700

    dasar

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+		
+		<link rel="shortcut icon" href="img/favicon.png"> 
+		
+		<link rel="stylesheet" type="text/css" href="css/reset.css" />
+		<link rel="stylesheet" type="text/css" href="simdesa-master/css/style.css" />
+		<link rel="stylesheet" type="text/css" href="css/style-responsive.css" />				
+		
+</head>
+
+<body>
+
+<div class="container">
+
+
+<!-- section start-page -->
+
+  <section class="start-page parallax-background" id="home">
+  
+    <div class="opacity"></div>
+      <div class="content">
+        <div class="text">
+          
+        </div>
+        <div class="arrow-down"></div>
+      </div>
+  
+  </section>
+
+   
+   
+  <!-- section menu -->
+  
+  <section class="menu">
+
+    <div class="menu-content">
+    
+    <div class="logo"></div>
+    
+    </div>
+  
+  
+  </section>
+
+
+  <!-- section about us -->
+  
+  <section class="about-us" id="about-us">
+  
+    <h1>MASUK SEBAGAI</h1>
+    <hr/>
+  <a href="home.php"/>
+      <div class="column-one">
+  
+        <div class="circle-one" ></div> </a>
+    
+          <h2>USER</h2>
+          
+    
+      </div>
+      
+      <div class="column-three">
+  
+        
+      </div>
+  
+   <a href="simdesa-master/login.php" >
+      <div class="column-two">
+ 
+        <div class="circle-two"></div>
+    </a>
+          <h2>ADMIN</h2>
+         
+      </div>
+
+  </section>
+  
+
+
+  
+  <div class="clear"></div>  
+
+
+	<!-- Scripts -->
+	
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script> <!-- jQuery -->
+	<script src="js/jquery.parallax.js"></script> <!-- jQuery Parallax -->
+	<script src="js/jquery.nicescroll.js"></script> <!-- jQuery NiceScroll -->
+	<script src="js/jquery.sticky.js"></script> <!-- jQuery Stick Menu -->
+	<script src="js/script.js"></script> <!-- All script -->
+
+</body>
+
+</html>
